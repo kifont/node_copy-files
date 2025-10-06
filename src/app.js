a4ad8ed3b42e9accdc7e60fd914481c720ba5c0c
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-10-06 17:01:12 +0300

    add solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,58 @@
+/* eslint-disable no-console */
 'use strict';
+
+import fs from 'fs';
+
+function copyFile() {
+  if (process.argv.slice(2).length !== 2) {
+    console.error('length is less');
+
+    return;
+  }
+
+  const [entryFile, copyTo] = process.argv.slice(2);
+
+  if (!entryFile || !copyTo) {
+    console.error('One or two param is undefined');
+
+    return;
+  }
+
+  if (entryFile === copyTo) {
+    return;
+  }
+
+  if (!fs.existsSync(entryFile)) {
+    console.error('Non-existent source file');
+
+    return;
+  }
+
+  const entryStats = fs.statSync(entryFile);
+
+  if (entryStats.isDirectory()) {
+    console.error('Source is a directory');
+
+    return;
+  }
+
+  if (fs.existsSync(copyTo)) {
+    const destStats = fs.statSync(copyTo);
+
+    if (destStats.isDirectory()) {
+      console.error('Destination is a directory');
+
+      return;
+    }
+  }
+
+  try {
+    fs.copyFileSync(entryFile, copyTo);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+copyFile();
+
+export default copyFile();
